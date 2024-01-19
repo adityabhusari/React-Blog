494eee5afde6b705daf90e0215afb5b9622f2c0b
aduspandu@gmail.com <aduspandu@gmail.com>
2024-01-20 01:11:44 +0530

    first

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width = "10%"}) {
+  return (
+    <img src='src/assets/Google-logo.png' style={{width}} alt='Logo placeholder' />
+  )
+}
+
+export default Logo
